5ddde2684025ce2b087e3b77e9671f20d6aef1c9
kylebirns <kylebirns@gmail.com>
2019-05-14 11:03:23 -0400

    Feat: more mutations and updating the apollo store

--- src/Components/LinkList.js
+++ src/Components/LinkList.js
@@ -1,37 +1,43 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Link from './Link'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
-// dummy data
-    // const linksToRender = [
-    //   {
-    //     id: '1',
-    //     description: 'Prisma turns your database into a GraphQL API 😎',
-    //     url: 'https://www.prismagraphql.com',
-    //   },
-    //   {
-    //     id: '2',
-    //     description: 'The best GraphQL client',
-    //     url: 'https://www.apollographql.com/docs/react/',
-    //   },
-    // ]
-
-function LinkList () {
-
-    const FEED_QUERY = gql`
-    {
-        feed {
-        links {
+export const FEED_QUERY = gql`
+{
+    feed {
+    links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
             id
-            createdAt
-            url
-            description
+          }
         }
-        }
+      }
     }
-    `
+}
+`
 
+class LinkList extends Component {
+
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY })
+  
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+  
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
+
+  render(){
     return (
         <Query query={FEED_QUERY}>
         {({ loading, error, data }) => {
@@ -42,12 +48,20 @@ function LinkList () {
     
           return (
             <div>
-              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+              {linksToRender.map((link, index) => (
+                <Link 
+                key={link.id} 
+                link={link} 
+                index={index}
+                updateStoreAfterVote={this._updateCacheAfterVote}
+                />
+              ))}
             </div>
           )
         }}
       </Query>
     )
   }
+}
 
 export default LinkList
