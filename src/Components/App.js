8bf9103412d5db46523aca14d574d270592a684d
kylebirns <kylebirns@gmail.com>
2019-05-15 11:25:07 -0400

    Feat: filtering with search component

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,6 +3,7 @@ import LinkList from './LinkList'
 import CreateLink from './CreateLink';
 import Login from './Login';
 import Header from './Header'
+import Search from './Search'
 import { Switch, Route } from 'react-router-dom'
 
 function App () {
@@ -14,6 +15,7 @@ function App () {
           <Route exact path="/" component={LinkList} />
           <Route exact path="/create" component={CreateLink} />
           <Route exact path="/login" component={Login} />
+          <Route exact path='/search' component={Search} />
         </Switch>
       </div>
     </div>
